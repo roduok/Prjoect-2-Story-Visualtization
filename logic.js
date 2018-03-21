526915aab634a3cc09434c0dfe9401706bdfee9d
roduok <30935021+roduok@users.noreply.github.com>
2018-03-20 22:05:19 -0400

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,75 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [40.7128, -74.0059],
+  zoom: 4
+});
+
+// Adding tile layer
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
+    "T6YbdDixkOBWH_k9GbS8JQ");
+
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
+    "T6YbdDixkOBWH_k9GbS8JQ");
+
+// Link to GeoJSON
+
+var geojson;
+
+// Grabbing data with d3...
+d3.json("static/zipcodedata.geojson", function(data) {
+
+  // Creating a new choropleth layer
+  geojson = L.choropleth(data, {
+    // Which property in the features to use
+    valueProperty: "NAICS2012_11",
+    // Color scale
+    scale: ["#ffffb2", "#b10026"],
+    // Number of breaks in step range
+    steps: 10,
+    // q for quantile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.ZIPCODE + " " + feature.properties.STATE + "<br>Number of Establishments:<br>" +
+        feature.properties.NAICS2012_11);
+    }
+  }).addTo(myMap);
+
+  // Setting up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Median Income</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  //Adding legend to the map
+  legend.addTo(myMap);
+
+});
